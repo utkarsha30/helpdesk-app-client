aabf64496b7f4f2d62418cd1f81e3625ca08749b
Utkarsha Kshirsagar <utkarshakshirsagar4717@gmail.com>
2023-01-12 02:29:04 +0530

    url

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-// const BASE_URL = `https://helpdesk-app-server.onrender.com`;
-const BASE_URL = `http://localhost:5000`;
+const BASE_URL = `https://helpdesk-app-server.onrender.com`;
+// const BASE_URL = `http://localhost:5000`;
 export { BASE_URL };
